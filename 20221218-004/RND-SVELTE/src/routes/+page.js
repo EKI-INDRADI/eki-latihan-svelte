18a33f586102e0fb30d1b1d90322e425ad3aec2a
EKI-INDRADI <eki.indradi@gmail.com>
2022-12-18 13:52:08 +0700

    update axios depdency load sveltkit 1.0

--- 20221218-004/RND-SVELTE/src/routes/+page.js
+++ 20221218-004/RND-SVELTE/src/routes/+page.js
@@ -1,63 +1,86 @@
 // function yang pertama kali di load sebelum load content  
 // kalo di angular namnya contructor atau bs juga form_load
 
+import axios from 'axios';
 /**
  * @param {any} event_load
  */
 export async function load(event_load) {
-    let getRecipesurl = 'http://127.0.0.1:3000/produk?limit=10'
+    // let getRecipesurl = 'http://127.0.0.1:3000/produk?limit=10'
+    // // https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API/Using_Fetch
+    // let getRecipesres = await fetch(getRecipesurl, {
+    //     method: 'GET', // *GET, POST, PUT, DELETE, etc.
+    //     // mode: 'cors', // no-cors, *cors, same-origin
+    //     // cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+    //     // credentials: 'same-origin', // include, *same-origin, omit
+    //     headers: {
+    //         'Content-Type': 'application/json',
+    //         Authorization: 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MjAyMjEyMTYwNzI3MDc4LCJ1c2VyX3BheWxvYWQiOnsiX2lkIjoiNjM5YzVmN2Y2OGE5NDIwOTJlYTQ1ZmRmIiwicGFzc3dvcmQiOiIkMmIkMTAkSy5sWEE4d28vVWczVGoyRlhNbURoLjMzczY4OVNpOGVNNnQ1QWxwc3liUXFJWHJ1aDRINXEiLCJ1c2VybmFtZSI6ImVraXRlc3RpbmciLCJlbWFpbCI6ImVraXRlc3RpbmdAbWFpbC5jb20iLCJuYW1hX3VzZXIiOiJla2kgdGVzdGluZyIsInVwZGF0ZV9hdCI6IjIwMjItMTItMTZUMTI6MDc6MjcuMDc4WiIsImNyZWF0ZV9hdCI6IjIwMjItMTItMTZUMTI6MDc6MjcuMDc4WiIsImlkIjoyMDIyMTIxNjA3MjcwNzgsIl9fdiI6MH0sImlhdCI6MTY3MTMzOTMwMCwiZXhwIjoxNjcxMzgyNTAwfQ.djvdC_fcKFOqcjdgUQBvR6cKJTiC0pprJ7SF9lB0Spo'
+    //     },
+    //     // redirect: 'follow', // manual, *follow, error
+    //     // referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+    //     // body: JSON.stringify({})
+    // }).catch(console.error)
 
-    // https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API/Using_Fetch
-    let getRecipesres = await fetch(getRecipesurl, {
-        method: 'GET', // *GET, POST, PUT, DELETE, etc.
-        // mode: 'cors', // no-cors, *cors, same-origin
-        // cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
-        // credentials: 'same-origin', // include, *same-origin, omit
+
+    // let getRecipesResults = []
+    // try {
+    //     if (getRecipesres) {
+    //         getRecipesResults = await getRecipesres.json()
+    //         getRecipesResults = getRecipesResults.results
+    //     }
+    // } catch (error) {
+    //     console.log("getRecipesResults", error)
+    // }
+    // console.log("getRecipesResults" ,getRecipesResults)
+
+
+
+    //------------------ USING AXIOS
+
+    let set_skip = 0
+    let set_limit = 10
+    let set_token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MjAyMjEyMTYwNzI3MDc4LCJ1c2VyX3BheWxvYWQiOnsiX2lkIjoiNjM5YzVmN2Y2OGE5NDIwOTJlYTQ1ZmRmIiwicGFzc3dvcmQiOiIkMmIkMTAkSy5sWEE4d28vVWczVGoyRlhNbURoLjMzczY4OVNpOGVNNnQ1QWxwc3liUXFJWHJ1aDRINXEiLCJ1c2VybmFtZSI6ImVraXRlc3RpbmciLCJlbWFpbCI6ImVraXRlc3RpbmdAbWFpbC5jb20iLCJuYW1hX3VzZXIiOiJla2kgdGVzdGluZyIsInVwZGF0ZV9hdCI6IjIwMjItMTItMTZUMTI6MDc6MjcuMDc4WiIsImNyZWF0ZV9hdCI6IjIwMjItMTItMTZUMTI6MDc6MjcuMDc4WiIsImlkIjoyMDIyMTIxNjA3MjcwNzgsIl9fdiI6MH0sImlhdCI6MTY3MTMzOTMwMCwiZXhwIjoxNjcxMzgyNTAwfQ.djvdC_fcKFOqcjdgUQBvR6cKJTiC0pprJ7SF9lB0Spo'
+    let url_parameter = `http://127.0.0.1:3000/produk?skip=${set_skip}&limit=${set_limit}`
+    let result = null
+    let AxiosParams = {
+        method: 'get',
+        url: url_parameter,
         headers: {
-            'Content-Type': 'application/json',
-            Authorization: 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MjAyMjEyMTYwNzI3MDc4LCJ1c2VyX3BheWxvYWQiOnsiX2lkIjoiNjM5YzVmN2Y2OGE5NDIwOTJlYTQ1ZmRmIiwicGFzc3dvcmQiOiIkMmIkMTAkSy5sWEE4d28vVWczVGoyRlhNbURoLjMzczY4OVNpOGVNNnQ1QWxwc3liUXFJWHJ1aDRINXEiLCJ1c2VybmFtZSI6ImVraXRlc3RpbmciLCJlbWFpbCI6ImVraXRlc3RpbmdAbWFpbC5jb20iLCJuYW1hX3VzZXIiOiJla2kgdGVzdGluZyIsInVwZGF0ZV9hdCI6IjIwMjItMTItMTZUMTI6MDc6MjcuMDc4WiIsImNyZWF0ZV9hdCI6IjIwMjItMTItMTZUMTI6MDc6MjcuMDc4WiIsImlkIjoyMDIyMTIxNjA3MjcwNzgsIl9fdiI6MH0sImlhdCI6MTY3MTE5NDQ0NCwiZXhwIjoxNjcxMjM3NjQ0fQ.3jT1z84_-ffvPiNkdhMBUDwatArBTdpndAF-YC-gFbg'
+            "Authorization": `Bearer ${set_token}`,
+            "Access-Control-Allow-Origin": "*"
         },
-        // redirect: 'follow', // manual, *follow, error
-        // referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
-        body: JSON.stringify({})
-    }).catch(console.error)
-
-
-    let getRecipesResults = []
-
-    try {
-
-        if (getRecipesres) {
-            getRecipesResults = await getRecipesres.json()
-            getRecipesResults = getRecipesResults.results
-        }
-
-    } catch (error) {
-        console.log("getRecipesResults", error)
+        data: {}
     }
 
-    console.log("getRecipesResults" ,getRecipesResults)
+    let result_data = null
+    let result_status = null
+    let res_json = {}
+    res_json.statusCode = 404
+    res_json.message = 'unkown error'
+    try {
+        result = await axios(AxiosParams)
+        result_data = result.data
+        result_status = result.status
+        res_json.statusCode = result_status
+        res_json.message = "SUCCESS"
+        res_json.response = result_data
+    } catch (error2) { // re-try + error info
+        result = await axios(AxiosParams).catch(async (error) => {
+            result_data = (error && error.response && error.response.data) ? error.response.data : null
+            result_status = (error && error.response && error.response.status) ? error.response.status : null
+            res_json.statusCode = result_status
+            let token_message = (error && error.response && error.response.data && error.response.data.error) ? error.response.data.error : "error unkown" //"ERROR"
+            res_json.message = `Error ${token_message}, unkown error` //"ERROR"
+            res_json.response = result_data
+        })
+    }
 
-
-
-    // const getRecipesurl2 = 'https://api.spoonacular.com/recipes/complexSearch?apiKey=b1c0836dd2e742f0af3199712142aee8'
-
-    // let getRecipesResults2 = []
-    // try {
-    //     const getRecipesres2 = await fetch(getRecipesurl2)
-    //     // const {results}  = await getRecipesResults.json()  // ==  getRecipesResults.results
-
-    //     getRecipesResults2 = await getRecipesres2.json()
-    //     getRecipesResults2 = getRecipesResults2.results
-    // } catch (error) {
-    //     console.log("getRecipesurl2" , error)
-
-    // }
-
+    //------------------ USING AXIOS
 
 
     return {
-        data: getRecipesResults,
+        product_res_json: res_json,
         // recipes2: getRecipesResults2
         // recipes: [
         //     {
@@ -67,3 +90,4 @@ export async function load(event_load) {
         // ]
     }
 }
+
--- 20221218-004/RND-SVELTE/src/routes/+page.svelte
+++ 20221218-004/RND-SVELTE/src/routes/+page.svelte
@@ -10,45 +10,41 @@
 	 * @type {any[]}
 	 */
 	export let data; // export let data;  get from +page.js // <---- return { recipes: results }
+
+	/**
+	 * @type {any[]}
+	 */
 	// @ts-ignore
-	//  const { recipes } =  data
-	data = data.recipes;
+	let product_res_json = data['product_res_json'];
 	// @ts-ignore
-	// const recipes2 = data.recipes2;
+	let product_res_json_data = product_res_json['response']['data']
 
 	onMount(async () => {});
+
+
 </script>
 
 <!--------------------- GUNAKAN MODULE SETELAH BERHASIL HTTP REQUEST (data) -->
 <!-- function yang pertama kali di load sebelum load content  
      kalo di angular namnya contructor atau bs juga form_load -->
-
-
-
 <!--------------------- GUNAKAN MODULE SETELAH BERHASIL HTTP REQUEST (data) -->
 
-
 <!--- FOR SUBSCRIBE / AXIOS / HTTP REQUEST USING onMount-->
-
 <!-- <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p> -->
 
-<h1>Food Recepies</h1>
+<h1>Product</h1>
 <!--<<<-----  USING LOAD +page.js (CONTRUCTOR/FORM LOAD)-->
 <!-- --------------------- RESPONSE HTTP REQUEST -->
-{#each data as item}
+
+{#each product_res_json_data as item}
 	<div>
-		<p>{item.nama_produk}</p>
-        <p>{item.deskripsi_produk}</p>
-        <p>{item.harga_jual}</p>
-        <p>{item.harga_beli}</p>
-		<img src={item.foto} alt={item.foto} />
+		<p>Nama product : {item.nama_produk}</p>
+		<p>Deskripsi product : {item.deskripsi_produk}</p>
+		<p>harga jual : {item.harga_jual}</p>
+		<p>harga beli : {item.harga_beli}</p>
+		<!-- <img src={item.foto} alt={item.foto} /> -->
 	</div>
 {/each}
 
-<!-- {#each recipes2 as item}
-	<div>
-		{item.id}
-	</div>
-{/each} -->
 <!-- --------------------- RESPONSE HTTP REQUEST -->
